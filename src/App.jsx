ba00d002ff17068a4dfe4b4746572684e8b74df8
Praanshu Grover <praanshugrover60@gmail.com>
2024-11-12 02:19:51 +0530

    Creates custom hooks for pizza of the day and display pizza of the day on home page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+
 import Order from "./order";
+import PizzaOfTheDay from "./PizzaOfTheDay";
 
 const App = () => {
   return (
-    <div>
-      <h1> Padre Gino's - Order Now</h1>
-      <Order />
-    </div>
+    <StrictMode>
+      <div>
+        <h1 className="logo"> Padre Gino's - Order Now</h1>
+        <Order />
+        <PizzaOfTheDay />
+      </div>
+    </StrictMode>
   );
 };
 
